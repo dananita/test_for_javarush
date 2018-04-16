9ac15bcbb21af542f1b399f92308288d3b12a6c1
dananita <zlakdanata@yandex.ru>
2018-04-16 09:33:13 +0300

    project initialization

---
+++ src/main/java/ru/octava/javarushtest/JavarushtestApplication.java
@@ -0,0 +1,12 @@
+package ru.octava.javarushtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavarushtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavarushtestApplication.class, args);
+    }
+}
